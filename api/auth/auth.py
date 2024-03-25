0157ec13e912a4c0e6cfea700172270e02f079e3
nnn1k <nikitos.elidu@gmail.com>
2024-03-25 23:05:55 +0300

    разобраться с запросом

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -9,6 +9,7 @@ auth = Blueprint('auth', __name__, url_prefix='/api/auth')
 user_valid = UserValidation()
 
 @auth.post('/registration')
+
 def register_user():
     Auth = AuthAPI()
     login = request.json['login']
@@ -31,15 +32,17 @@ def register_user():
 
 
 @auth.post('/login')
+@cross_origin()
 def login_user():
     Auth = AuthAPI()
     login = request.json['login']
     password = request.json['password']
     Auth.login(login, password)
     print(request.json)
+    print(Auth.error)
     if Auth.error == 'Нет такого пользователя':
-        abort(401)
-        # return jsonify(error="Неверный логин или пароль")
+        #abort(401)
+        return jsonify(error="Неверный логин или пароль")
 
     user = Auth.user.__dict__
     token = create_token({'id': user['id']})
